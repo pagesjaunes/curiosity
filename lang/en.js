222475c2b3aef55b1050d75908150f3f7ab2d4c3
Arthur BABEY <ababey@pagesjaunes.fr>
2014-05-13 15:30:12 +0200

    Traduction des texts en français + mise en fichier de conf des textes liés a l'application

---
+++ lang/en.js
@@ -0,0 +1,84 @@
+var global_text = {
+	header : {
+		Server : "Server",
+		New : "New",
+		AddServer : "Add a new server",
+		Connect : "Connect",
+		Index : "Index",
+		Search : "Search",
+		AutoRefresh : "AutoRefresh",
+		Hits : "Hits : ",
+		Error : "Error",
+		navBar : {
+			Request : "Request",
+			Aggregations : "Aggregations",
+			Results : "Results",
+			Mapping : "Mapping",
+			Keywords : "Keywords",
+			Templates : "Templates"
+		}
+	},
+	aggregation : {
+		Fields : "Fields",
+		aggregation_renderer : {
+			Type : "Type",
+			Field : "Field"
+		}
+	},
+	keyword : {
+		Index : "Index",
+		Save : "Save", 
+		Add : "Add"
+	},
+	mapping : {
+		DocumentT : "Document Type"
+	},
+	request : {
+		SimplifiedRequest:"Simplified Request",
+		ComplexeRequest:"Complexe Request",
+		ResultPerPage:"Result per page",
+		Search:"Search",
+		JsonQuery: "Json Query",
+		Keywords: "Keywords",
+		Fields:"Fields"
+	},
+	result : {
+		request_sub_tab : {
+			Template : "Template",
+			First: "First",
+			Prev: "Prev",
+			Next: "Next",
+			Last: "Last",
+			Page: "Page",
+			Go : "Go",
+			AggregationFilter : "Aggregation Filter",
+			Or:"Or",
+			And:"And",
+			Not:"Not", 
+		}
+	},
+	template : {
+		ResultItems : "Result Items",
+		Aggregations : "Aggregations",
+		aggregation_sub_tab :{
+			Association : "Association",
+			AggregationType : "Aggregation Type",
+			Template : "Template",
+			Default : "Default",
+			Update : "Update",
+			Edit: "Edit",
+			New: "New",
+			Save: "Save",
+			Delete: "Delete",
+			TemplateName : "Template's name"
+		},
+		result_items_sub_tab :{
+			Template : "Template",
+			New: "New",
+			Save: "Save",
+			Delete: "Delete",
+			TemplateName : "Template's name",
+			Fields : "Field"
+		}
+	}
+}
---
+++ lang/fr.js
@@ -0,0 +1,84 @@
+var global_text = {
+	header : {
+		Server : "Serveur",
+		New : "Nouveau",
+		AddServer : "Ajouter un nouveau serveur",
+		Connect : "Connexion",
+		Index : "Index",
+		Search : "Rechercher",
+		AutoRefresh : "Raffraichissement auto.",
+		Hits : "Résultats : ",
+		Error : "Erreur",
+		navBar : {
+			Request : "Requête",
+			Aggregations : "Aggrégations",
+			Results : "Résultas",
+			Mapping : "Mapping",
+			Keywords : "Mots Clés",
+			Templates : "Templates"
+		}
+	},
+	aggregation : {
+		Fields : "Champs",
+		aggregation_renderer : {
+			Type : "Type",
+			Field : "Champ"
+		}
+	},
+	keyword : {
+		Index : "Index",
+		Save : "Sauvegarder", 
+		Add : "Ajouter"
+	},
+	mapping : {
+		DocumentT : "Type de document"
+	},
+	request : {
+		SimplifiedRequest:"Requête simplifiée",
+		ComplexeRequest:"Requête compléte",
+		ResultPerPage:"Resultas par page",
+		Search:"Rechercher",
+		JsonQuery: "Requête Json",
+		Keywords: "Mots Clés",
+		Fields:"Champs"
+	},
+	result : {
+		request_sub_tab : {
+			Template : "Template",
+			First: "Première",
+			Prev: "Précédente",
+			Next: "Suivante",
+			Last: "Dernière",
+			Page: "Page",
+			Go : "Aller à",
+			AggregationFilter : "Filtres d'aggrégation",
+			Or:"Ou",
+			And:"Et",
+			Not:"Non", 
+		}
+	},
+	template : {
+		ResultItems : "Objets Resultas",
+		Aggregations : "Aggrégations",
+		aggregation_sub_tab :{
+			Association : "Association",
+			AggregationType : "Type d'aggrégation",
+			Template : "Template",
+			Default : "Defaut",
+			Update : "Mettre à jour",
+			Edit: "Edition",
+			New: "Nouveau",
+			Save: "Sauvegarder",
+			Delete: "Supprimer",
+			TemplateName : "Nom du Template"
+		},
+		result_items_sub_tab :{
+			Template : "Template",
+			New: "Nouveau",
+			Save: "Sauvegarder",
+			Delete: "Supprimer",
+			TemplateName : "Nom	du Template",
+			Fields : "Champs"
+		}
+	}
+}
