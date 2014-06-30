8cec260aac3ab0f19951c2f3a745d2b8605af25c
Arthur BABEY <ababey@pagesjaunes.fr>
2014-06-30 12:06:14 +0200

    it will work

--- INT.conf.js
+++ INT.conf.js
@@ -1,6 +1,6 @@
 var globalConf = {
     defaultServer : 'http://bigdatasearchvg.pi1g.local:10200', 
-    confServer : 'http://ioda01t.bbo1t.local:10200',
+    confServer : 'http://exalead1t.bbo1t.local:10200',
     confIndex : "curiosity-config",
     defaultConfDocumentType : "conf-doc",
     gMappings : {},
