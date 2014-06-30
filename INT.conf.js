6fda7f9d70a384760186dd23bef28a73b66ad3c3
Arthur BABEY <ababey@pagesjaunes.fr>
2014-06-30 11:54:42 +0200

    fichier INT

---
+++ INT.conf.js
@@ -0,0 +1,10 @@
+var globalConf = {
+    defaultServer : 'http://bigdatasearchvg.pi1g.local:10200/', 
+    confServer : 'http://ioda01t.bbo1t.local:8090',
+    confIndex : "curiosity-config",
+    defaultConfDocumentType : "conf-doc",
+    gMappings : {},
+    aggregationArray : [],
+    gRequestResult : {},
+    version : "v0.1.0"
+}
