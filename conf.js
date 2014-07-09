81e8fef09cceebe2ca486a1c3fa88460c1dc39ba
Matthieu CAULE <mcaule@pagesjaunes.fr>
2014-07-09 15:49:52 +0200

    add barchart directive (dev state : hard coded data)

--- conf.js
+++ conf.js
@@ -1,6 +1,10 @@
 var globalConf = {
-    defaultServer : 'http://localhost:9200',
-    confServer : 'http://localhost:9200',
+    defaultServer : 'http://exalead1t.bbo1t.local:10200', 
+    confServer : 'http://exalead1t.bbo1t.local:10200',
     confIndex : "curiosity-config",
     defaultConfDocumentType : "conf-doc",
+    gMappings : {},
+    aggregationArray : [],
+    gRequestResult : {},
+    version : "v0.1.0"
 }
--- js/d3/directives/barChartDirective.js
+++ js/d3/directives/barChartDirective.js
@@ -1,69 +1,52 @@
 Curiosity.directive('barchart', function(){
+
+
 	return {
+		scope:{
+			data:"="
+		},
 		restrict : 'A',
-		link: function($scope, element, attrs) {
-			var dataset = [{key:"berry", doc_count:890}, {key:"toto", doc_count:155}, {key:"bibi", doc_count:6500},{key:"berry", doc_count:645},{key:"berry", doc_count:750}];
-			// TODO :  set width and height dynamicly
-			var margin = {top: 20, right: 20, bottom: 30, left: 40},
-			width = attrs.width - margin.left - margin.right,
-			height = attrs.height - margin.top - margin.bottom;
+		link: function($scope, elem, attrs) {
+			console.log('barchart',$scope.data)
 
-			var x = d3.scale.ordinal()
-			.domain(d3.range(dataset.length))
-			.rangeRoundBands([0, width], .1)
 
-			var y = d3.scale.linear()
-			.domain([0, d3.max(dataset, function (d){return	d[attrs.abscissa]})])
-			.range([height, 0 ])
+			var seriesData = $scope.data.map(function(d)
+			{
+				return {
+					x:d._source.quiQuoi_normalized,
+					y:d._source.nb_requetes
+				}
+			})
 
-			var xAxis = d3.svg.axis()
-    		.scale(x)
-    		.orient("bottom");
+			var data = [{key:'nb_requetes',values:seriesData}]
 
-			var yAxis = d3.svg.axis()
-    		.scale(y)
-    		.orient("left")
-    		.ticks(10);
 
-			/* xScale initialisation
-			var xAxis = d3.scale.ordinal()
+
+			console.log(data)
+
+			var w = $(elem).width()
+			var h = w*2/3
+			console.log('elem.load',w,h)
+			var chart = nv.models.multiBarChart()
+							.width(w)
+							.height(h)
+							.margin({top:20,right:30,bottom:50,left:30})
+
+			nv.utils.windowResize(chart.update)
+
+			d3.select(elem[0])
+				.append('svg')
+				.attr('width',w)
+				.attr('height',h)
+				.style('width',w)
+				.style('height',h)
+				.attr('class','barchart')
+				.datum(data)
+				.call(chart)			
+
+
 			
 
-			 yScale initialisation
-			var yAxis = d3.scale.linear()
-			*/
-
-			//Create SVG element
-			var svg = d3.select(element[0])
-			.append("svg")
-			.attr("width", width)
-			.attr("height", height);
-
-			svg.append("g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
-			.call(xAxis);
-
-			svg.append("g")
-			.attr("class", "y axis")
-			.call(yAxis)
-			.append("text")
-			.attr("transform", "rotate(-90)")
-			.attr("y", 6)
-			.attr("dy", ".71em")
-			.style("text-anchor", "end")
-			.text(attrs.abscissa);
-
-			svg.selectAll(".bar")
-			.data(dataset)
-			.enter().append("rect")
-			.attr("class", "bar")
-			.attr("x", function(d, i) {
-				return x(i); })
-			.attr("width", x.rangeBand())
-			.attr("y", function(d) { 
-				return y(d.doc_count); })
-			.attr("height", function(d) { return height - y(d[attrs.abscissa]); });
 		}
 	}
 });
