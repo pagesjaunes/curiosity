d18998892ba9187a806234622fd796ef8dfa9a39
Matthieu CAULE <mcaule@pagesjaunes.fr>
2014-07-09 18:05:33 +0200

    add pieChart directive

---
+++ js/d3/directives/pieChartDirective.js
@@ -0,0 +1,94 @@
+Curiosity.directive('piechart', function($rootScope){
+	// Runs during compile
+	return {
+		restrict :'A',
+		
+		scope: {
+			datax :"=",
+			datay :"=",
+			pathx: "=",
+			pathy: "=",
+		},		
+		
+		link: function($scope, elem, iAttrs) {
+			if (typeof ($scope.datay) === "undefined") {
+				$scope.datay = $scope.datax
+			} 			
+
+			var data = builtAllSeries($scope.datax, $scope.pathx, $scope.datay, $scope.pathy);
+			data = data.pop().values
+			var w = $(elem).width()
+			var h = w
+			console.log('piechart',data)
+			var chart = nv.models.pieChart()
+							.x(function(d) { return d.x })
+							.y(function(d) { return d.y })
+							.width(w)
+							.height(h)
+							.margin({top:20,right:20,bottom:20,left:20})
+							//.showLabels(true)
+			if(w<150)
+			{
+				chart.showLegend(false)
+					 .showLabels(true)
+			}
+
+			nv.utils.windowResize(chart.update)
+
+
+
+			d3.select(elem[0])
+				.append('svg')
+				.attr('width',w)
+				.attr('height',h)
+				.style('width',w)
+				.style('height',h)
+				.attr('class','piechart')
+				.datum(data)
+				.call(chart)			
+
+			function getData(dataSet, path, num) {
+				var i = 0;
+				while (i < path.length) {		
+					if (typeof dataSet[path[i]] === "undefined")
+						return (null);
+					dataSet = dataSet[path[i]];
+					i++;
+				}
+				if (num) {
+					return (parseFloat(dataSet)); 
+				}
+				return (dataSet);
+			}
+
+			function builtSerie(datax, pathx, datay, pathy) {
+				var res = {};
+				res.key = pathy[pathy.length -1];
+				var i = 0;
+				res.values = [];
+				while (i < datax.length) {
+					var tmp = {};
+					tmp.x = getData(datax[i], pathx);
+					tmp.y = getData(datay[i], pathy, true);
+					res.values.push(tmp);
+					i++;
+				}
+				return (res);
+			}
+
+			function builtAllSeries(datax, pathx, datay, pathsy) {
+				var res = [];
+				var i = 0;
+				var pathxArray = pathx.split('.'); 
+				while (i < pathsy.length) {
+					var tmp = builtSerie(datax, pathxArray, datay, pathsy[i].split('.'));
+					res.push(tmp);
+					i++;					
+				}
+				console.log(res);
+				return (res);
+			}
+		}
+	};
+});
+
