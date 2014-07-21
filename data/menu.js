5cd55b8a5ec2ee5991c92163819f2a24f0d72e31
Arthur BABEY <ababey@pagesjaunes.fr>
2014-07-21 16:35:06 +0200

    Revert "update link"
    This reverts commit 623e56b327d1ebd9d38400653d0742cccdb9bd2b.

--- data/menu.js
+++ data/menu.js
@@ -3,7 +3,7 @@ var menu =
 	{"txt":"Getting Started", "url":"/curiosity/Getting-Started", "title":"Getting Started with Curiosity", "desc":"Link to the page Getting Started"},
 	{"txt":"Keywords and Advanced Query", "url":"/curiosity/Keywords-and-Advanced-Query", "title":"Documentation about Keywords and Advanced Query in Curiosity", "desc":"Link to the page Keywords and Advanced Query"},
 	{"txt":"Aggregations", "url":"/curiosity/Aggregations", "title":"Aggregations Modules Documentation", "desc":"Link to the page Aggregations"},
-	{"txt":"Templating", "url":"/curiosity/Templating", "title":"Results and Aggregations templating in Curiosity", "desc":"Link to the page Templating"},
+	{"txt":"Templating", "url":"/Tcuriosity/emplating", "title":"Results and Aggregations templating in Curiosity", "desc":"Link to the page Templating"},
 	{"txt":"Modules", "url":"/curiosity/Modules", "title":"Presentation of the Curiosity's modules Organisation", "desc":"Link to the page Modules"},
 	{"txt":"Contexts", "url":"/curiosity/Contexts", "title":"Presentation and explanation of Curiosity's search Contexts", "desc":"Link to the page Contexts"}
 ]
