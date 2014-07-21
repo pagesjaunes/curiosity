8d6cde7c624919ba84ff938e6dc410c0ebd7f4ee
Arthur BABEY <ababey@pagesjaunes.fr>
2014-07-21 16:35:11 +0200

    Revert "update link"
    This reverts commit 85a7dd7f92770b92c40eea8d02c3229a1be2945b.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 var app = angular.module('curiosityDoc', ['ngRoute'])
 .config(function($routeProvider, $locationProvider) {
 	$routeProvider
-	.when('/curiosity/', {
+	.when('/', {
 		templateUrl: 'curiosity/partials/home.html',
 	})
-	.when('/curiosity/Getting-Started', {
+	.when('/Getting-Started', {
 		templateUrl: 'curiosity/partials/start.html',
 	})
-	.when('/curiosity/Keywords-and-Advanced-Query', {
+	.when('/Keywords-and-Advanced-Query', {
 		templateUrl: 'curiosity/partials/keywords.html',
 	})
-	.when('/curiosity/Templating', {
+	.when('/Templating', {
 		templateUrl: 'curiosity/partials/templating.html',
 	})
-	.when('/curiosity/Aggregations', {
+	.when('/Aggregations', {
 		templateUrl: 'curiosity/partials/aggregations.html',
 	})
-	.when('/curiosity/Modules', {
+	.when('/Modules', {
 		templateUrl: 'curiosity/partials/modules.html',
 	})
-	.when('/curiosity/Contexts', {
+	.when('/Contexts', {
 		templateUrl: 'curiosity/partials/context.html',
 	});
 	$locationProvider.html5Mode(true);
