f3064dea2f32106e069ff821b14fb70e7cd76587
Arthur BABEY <ababey@pagesjaunes.fr>
2014-07-21 16:35:12 +0200

    Revert "update link"
    This reverts commit 6dec507dba90f83b9b25366c8e049e53ab1979df.

--- js/app.js
+++ js/app.js
@@ -2,25 +2,25 @@ var app = angular.module('curiosityDoc', ['ngRoute'])
 .config(function($routeProvider, $locationProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: 'curiosity/partials/home.html',
+		templateUrl: './partials/home.html',
 	})
 	.when('/Getting-Started', {
-		templateUrl: 'curiosity/partials/start.html',
+		templateUrl: './partials/start.html',
 	})
 	.when('/Keywords-and-Advanced-Query', {
-		templateUrl: 'curiosity/partials/keywords.html',
+		templateUrl: './partials/keywords.html',
 	})
 	.when('/Templating', {
-		templateUrl: 'curiosity/partials/templating.html',
+		templateUrl: './partials/templating.html',
 	})
 	.when('/Aggregations', {
-		templateUrl: 'curiosity/partials/aggregations.html',
+		templateUrl: './partials/aggregations.html',
 	})
 	.when('/Modules', {
-		templateUrl: 'curiosity/partials/modules.html',
+		templateUrl: './partials/modules.html',
 	})
 	.when('/Contexts', {
-		templateUrl: 'curiosity/partials/context.html',
+		templateUrl: './partials/context.html',
 	});
 	$locationProvider.html5Mode(true);
 });
