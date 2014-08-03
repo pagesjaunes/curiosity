d219b241a151482fa49c8f04be65846be7bbc382
epigneul <erwan.pigneul@gmail.com>
2014-08-03 23:53:42 +0200

    localhost by default

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var globalConf = {
-    defaultServer : 'http://bigdatasearchvg.pi1g.local:10200', 
-    confServer : 'http://exalead1t.bbo1t.local:10200',
+    defaultServer : 'http://localhost:9200', 
+    confServer : 'http://localhost:10200',
     confIndex : "curiosity-config",
     defaultConfDocumentType : "conf-doc",
 }
