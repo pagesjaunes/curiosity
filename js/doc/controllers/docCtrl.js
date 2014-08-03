82acbbde240db5f600fbc08a2d9499331fb713b7
epigneul <erwan.pigneul@gmail.com>
2014-08-04 00:32:44 +0200

    localhost by default

--- js/doc/controllers/docCtrl.js
+++ js/doc/controllers/docCtrl.js
@@ -2,9 +2,10 @@
 * @desc document module's controller. Hiden module used to modify data on elasticsearch server, onli for test and advanced users
 * more functionality in incoming versions
 */
-Curiosity.controller('docCtrl', function($scope, doc){
+Curiosity.controller('docCtrl', function($scope, doc, query){
 
 	$scope.info = doc.data;
+	$scope.queryData = query.info;
 
 	/**
 	* @desc delete a document 
@@ -14,4 +15,34 @@ Curiosity.controller('docCtrl', function($scope, doc){
 	$scope.deleteDocument = function (id, type) {
 		doc.deleteDocument(id, type);
 	}
+
+	/**
+	* @desc update a elasticsearch document
+	* @param string id elastcsearch document's id
+	* @param string type document's type
+	* @param json doc to update
+	*/
+	$scope.updateDocument = function (id, type, source) {
+		doc.updateDocument = function (id, type, source);
+	}
+
+	/**
+	* @desc clone a elasticsearch document
+	* @param string id elastcsearch document's id to clone
+	* @param string type document's type to clone
+	* @param string id elastcsearch document's id to create
+	* @param string type document's type to create
+	*/
+	$scope.cloneDocument = function (fromId, fromType, toId, toType) {
+		doc.cloneDocument = function (fromId, fromType, toId, toType);
+	}
+
+	/**
+	* @desc generate an id. Can be usefull to clone a doc
+	*/
+	$scope.generateId = function () {
+		return doc.generateId();
+	};
+
+
 });
--- js/doc/services/docFactory.js
+++ js/doc/services/docFactory.js
@@ -26,5 +26,46 @@ Curiosity.factory('doc', function(elasticClient, curiosity, elasticFunc, $rootSc
 		elasticFunc.deleteDocument(client, currentIndex, type, id);
 	}
 
+	/**
+	* @desc update a elasticsearch document
+	* @param string id elastcsearch document's id
+	* @param string type document's type
+	* @param json doc to update
+	*/
+	obj.updateDocument = function (id, type, source) {
+		elasticFunc.sendDocument(client, currentIndex, type, source, id);
+	}
+
+	/**
+	* @desc clone a elasticsearch document
+	* @param string id elastcsearch document's id to clone
+	* @param string type document's type to clone
+	* @param string id elastcsearch document's id to create
+	* @param string type document's type to create
+	*/
+	obj.cloneDocument = function (fromId, fromType, toId, toType) {
+		client.get({index:currentIndex, type:fromType, id:fromId})
+			.then(function(data) {
+				docToClone = data.hits.hits[0];
+				if (typeof (docToClone) === "undefined")  { 				// Doc not found
+					log.log("Clone a doc : Error : doc " + fromId + " not found", "danger");
+				}
+				else {													// Context found
+					elasticFunc.sendDocument(client, currentIndex, toType, docToClone._source, toId);
+				}
+			}		
+	}
+
+	/**
+	* @desc generate an id. Can be usefull to clone a doc
+	*/
+	obj.generateId = function () {
+		// Math.random should be unique because of its seeding algorithm.
+		// Convert it to base 36 (numbers + letters), and grab the first 9 characters
+		// after the decimal.
+		return '_' + Math.random().toString(36).substr(2, 9);
+	};
+
+
 	return (obj);
 });
