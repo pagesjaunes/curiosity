95e0c4619d97a197435b6afb42a662e090b9aa2c
Erwan Pigneul <erwan.pigneul@gmail.com>
2014-08-04 00:33:32 +0200

    Oubli du chnagement de port

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var globalConf = {
     defaultServer : 'http://localhost:9200', 
-    confServer : 'http://localhost:10200',
+    confServer : 'http://localhost:9200',
     confIndex : "curiosity-config",
     defaultConfDocumentType : "conf-doc",
 }
