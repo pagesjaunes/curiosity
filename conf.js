988bafa11ccfd33ed8029d406e7fa2263a1d7201
Erwan Pigneul <epigneul@pagesjaunes.fr>
2014-09-19 17:38:10 +0200

    And a default notebook on ExploreContext

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 var globalConf = {
-    defaultServer : 'http://localhost:9200', 
-    confServer : 'http://localhost:9200',
-    confIndex : "curiosity-config",
+    defaultServer : 'http://bigdatasearchvg.pi1g.local:10200', 
+    confServer : 'http://bigdatasearchvg.pi1g.local:10200',
+    confIndex : "curiosity-config-erwan-dernier-test",
     defaultConfDocumentType : "conf-doc",
     defaultContextDocumentType : "context-doc",
     readonlyContext : ["exploreContext","basicSearch"],
