6c6e53dd02f531d82bb3d3bafb979054c29d2bef
fabiolab <fabypubs@yahoo.fr>
2014-10-01 09:02:42 +0200

    Delete INT.conf.js

--- INT.conf.js
+++
@@ -1,6 +0,0 @@
-var globalConf = {
-    defaultServer : 'http://bigdatasearchvg.pi1g.local:10200', 
-    confServer : 'http://bigdatasearchvg.pi1g.local:10200',
-    confIndex : "curiosity-config",
-    defaultConfDocumentType : "conf-doc"
-}
