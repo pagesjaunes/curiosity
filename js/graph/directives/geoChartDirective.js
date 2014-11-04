c305ab6af9f995464d41f9ecb2a44e47d7f870e8
epigneul <erwan.pigneul@gmail.com>
2014-11-04 01:03:04 +0100

    add new graphe features : diff and geocharts

---
+++ js/graph/directives/geoChartDirective.js
@@ -0,0 +1,63 @@
+Curiosity.directive('geochart', function($rootScope, filters){
+	// Runs during compile
+	return {
+		restrict :'A',
+		
+		scope: {
+			'data' :"=",
+			'cols' :"=",
+			'options': '=',
+			'agg' : "="
+		},			
+		
+		link: function($scope, elem, iAttrs) {
+			var options = {};
+			if (typeof $scope.options !== "undefined") {
+				options = $scope.options
+			}
+
+			var data = new google.visualization.DataTable({
+				cols: $scope.cols,
+				rows: builtDataFromCols($scope.data, $scope.cols)
+   			});
+
+			var chart = new google.visualization.GeoChart(elem[0]);
+			
+			$(elem[0]).height("100%");
+			console.log(data);
+			console.log(options);
+			chart.draw(data, options);
+			
+			function selectHandler(){
+				var select = chart.getSelection();
+				var  i = 0;
+				while (i < select.length) {
+					item = select[i];
+					if (item.row != null) {
+						filters.addFilter({type:'Terms', data:{field:$scope.agg.__ref__.field, term:data.getFormattedValue(item.row, 0)}});
+					}
+					i++;
+				}			
+			}
+
+			// agg filters
+			if (typeof $scope.agg !== "undefined")  {
+				google.visualization.events.addListener(chart, 'select', selectHandler);	
+			}
+
+			$scope.$watch('data',updateChart);
+			$scope.$on("workspaceChange", updateChart);
+			function updateChart() {	
+				if($(elem).is(":visible")) {					
+					data = new google.visualization.DataTable({
+						cols: $scope.cols,
+						rows: builtDataFromCols($scope.data, $scope.cols)
+					});
+					chart.draw(data, options);
+					console.log(data);
+				}
+			}
+		}
+	};
+});
+
